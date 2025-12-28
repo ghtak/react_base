9dcfd316d36281c47f289dc3b2388ac6ba244e7f
gwonhyeong.tak <gwonhyeong.tak@gmail.com>
2025-12-28 18:25:47 +0900

    feat: refactor Nav component to use state for dynamic link rendering

--- src/components/ui/nav.tsx
+++ src/components/ui/nav.tsx
@@ -1,13 +1,28 @@
-import styles from './styles/nav.module.scss';
-import { Link } from '@tanstack/react-router';
+import { useState } from "react";
+import styles from "./styles/nav.module.scss";
+import { Link } from "@tanstack/react-router";
 
 export function Nav() {
+  const [links] = useState([
+    {
+      name: "Home",
+      path: "/",
+    },
+    {
+      name: "About",
+      path: "/about",
+    },
+  ]);
+
+  const menus = links.map((link) => (
+    <li>
+      <Link to={link.path}>{link.name}</Link>
+    </li>
+  ));
+
   return (
     <nav className={styles.nav}>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-      </ul>
+      <ul>{menus}</ul>
     </nav>
   );
 }
